e6eaeee4146e3e2a26f873ebdcff35134f3a0b8b
ThorinGondor <1360002051@qq.com>
2023-11-12 13:25:02 +0800

    init commit

--- preface/lesson5.go
+++ preface/lesson5.go
@@ -8,7 +8,28 @@ func main() {
 	switch x {
 	case y, 16: // case 可以带多个表达式
 		fmt.Println("Yes")
-	default:
+	default: // default 代码块非必须
 		fmt.Println("Nope")
+	}
+
+	switch x {
+	case 16:
+		fmt.Println("case 1")
+		fallthrough // attention: 可以向下面的case穿透，即使不符合 case 里的表达式判断结果，默认只能穿透一层case
+	case 32:
+		fmt.Println("case 2")
+		fallthrough
+	case 64:
+		fmt.Println("case 3")
+	case 108:
+		fmt.Println("case 3")
+	}
+
+	var itf interface{}
+	switch variable := itf.(type) {
+	case int8:
+		fmt.Printf("type int8: %T", variable)
+	case nil:
+		fmt.Printf("type nil %T", variable)
 	}
 }
